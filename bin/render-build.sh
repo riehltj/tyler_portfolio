8c63c995ff1e019c1cc6e30d0ea1fb4b8e6cc4e2
Tyler Riehl <tyler@tryboxedup.com>
2025-07-02 08:17:41 -0600

    Updated build script to include building tailwind

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,8 @@ set -o errexit
 bundle install
 bundle exec rails db:migrate
 bundle exec rails db:seed
+bundle exec rails assets:clobber
 bin/rails tailwindcss:build
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake assets:precompile
+
 
